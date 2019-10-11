44ef5c10f582a3d1b857691371d627fa966d74db
prateek <proforty7@gmail.com>
2019-10-11 21:08:46 +0530

    Add view profile
    Signed-off-by: prateek <proforty7@gmail.com>

--- client/src/components/ViewProfile.jsx
+++ client/src/components/ViewProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import { withRouter } from "react-router-dom";
 import {
@@ -12,162 +12,189 @@ import {
   Button,
   Select,
   message,
-  Icon
+  Icon,
+  Badge,
+  Tag,
+  Divider,
+  Collapse
 } from "antd";
 import Container from "./Elements/Container";
 import { BRANCHES, LOCATIONS } from "../utils";
-import { createProfile, getProfileByUserId } from "../actions";
+import { getProfileByUserId } from "../actions";
 
 const ViewProfile = props => {
-  console.log(props);
+  const [profile, setProfile] = useState(null);
   const id = props.match.params.userId;
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getProfileByUserId(id);
+      setProfile(response.data);
     };
     fetchData();
   }, [id]);
-
   return (
     <Container>
-      {/* <Card>
-        <Row type="flex" align="middle">
-          <Col span={10}>
-            <div
-              style={{
-                textAlign: "center"
-              }}
+      {!profile ? (
+        <Card loading />
+      ) : (
+        <Card>
+          <Row type="flex" align="middle">
+            <Col span={10}>
+              <div
+                style={{
+                  textAlign: "center"
+                }}
+              >
+                <Avatar icon="user" size={160} />
+              </div>
+            </Col>
+            <Col span={14}>
+              <Typography.Title>{profile.user.name}</Typography.Title>
+              <div style={{ fontSize: 20 }}>@{profile.handle}</div>
+              <Divider />
+              <div>{profile.bio}</div>
+            </Col>
+          </Row>
+          <Row type="flex" justify="space-between" style={{ marginTop: 30 }}>
+            <Col span={11}>
+              <Badge
+                status="processing"
+                text={
+                  <span style={{ fontSize: 20 }}>
+                    <span style={{ fontWeight: "bold" }}>Status: </span>
+                    <span style={{ textTransform: "capitalize" }}>
+                      {profile.status}
+                    </span>
+                  </span>
+                }
+              />
+            </Col>
+            <Col span={11}>
+              {profile.githubusername ? (
+                <Badge
+                  status="processing"
+                  text={
+                    <span style={{ fontSize: 20 }}>
+                      <span style={{ fontWeight: "bold" }}>
+                        <Icon type="github" />
+                      </span>
+                      <span>{profile.githubusername}</span>
+                    </span>
+                  }
+                />
+              ) : null}
+            </Col>
+            <Col span={24} style={{ marginTop: 20, marginBottom: 20 }}>
+              <Badge
+                status="processing"
+                text={
+                  <span style={{ fontSize: 20 }}>
+                    <span style={{ fontWeight: "bold" }}>Skills: </span>
+                    <span>
+                      {profile.skills.map(skill => (
+                        <Tag
+                          color="blue"
+                          style={{
+                            fontSize: 16,
+                            padding: 7,
+                            borderRadius: 25
+                          }}
+                        >
+                          {skill}
+                        </Tag>
+                      ))}
+                    </span>
+                  </span>
+                }
+              />
+            </Col>
+            <Col span={24}>
+              <Badge
+                status="processing"
+                text={
+                  <span style={{ fontSize: 20 }}>
+                    <span style={{ fontWeight: "bold" }}>From: </span>
+                    <span>{profile.location}</span>
+                  </span>
+                }
+              />
+            </Col>
+            <Col span={24} style={{ marginTop: 30 }}>
+              <Collapse>
+                <Collapse.Panel header={<div>More Details</div>}>
+                  <div>
+                    <Badge
+                      status="processing"
+                      text={
+                        <span style={{ fontSize: 16 }}>
+                          <span style={{ fontWeight: "bold" }}>College: </span>
+                          <span>{profile.collegeID}</span>
+                        </span>
+                      }
+                    />
+                  </div>
+                  <div>
+                    <Badge
+                      status="processing"
+                      text={
+                        <span style={{ fontSize: 16 }}>
+                          <span style={{ fontWeight: "bold" }}>Branch: </span>
+                          <span>{profile.branch}</span>
+                        </span>
+                      }
+                    />
+                  </div>
+                </Collapse.Panel>
+              </Collapse>
+            </Col>
+            {/* <Col span={24}>
+              <Card
+                title={
+                  <div style={{ fontSize: 20 }}>
+                    <Icon type="book" style={{ marginRight: 15 }} />
+                    <Typography.Text>Academic Details</Typography.Text>
+                  </div>
+                }
+              >
+                <Form.Item>
+                  {getFieldDecorator("collegeID")(
+                    <Input size="large" placeholder="Enter your College ID" />
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator("branch")(
+                    <Select size="large" placeholder="Select your Branch">
+                      {BRANCHES.map(branch => (
+                        <Select.Option key={branch} value={branch}>
+                          {branch}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator("yearOfJoin")(
+                    <Input
+                      size="large"
+                      placeholder="Enter your Year of Joining (YYYY)"
+                    />
+                  )}
+                </Form.Item>
+              </Card>
+            </Col> */}
+            <Button
+              htmlType="submit"
+              type="primary"
+              shape="round"
+              size="large"
+              style={{ marginTop: 25 }}
             >
-              <Avatar icon="user" size={160} />
-            </div>
-          </Col>
-          <Col span={14}>
-            <Typography.Title>{user.name}</Typography.Title>
-            <Form.Item>
-              {getFieldDecorator("bio")(
-                <Input.TextArea placeholder="Add bio..." rows={5} />
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-
-        <Row type="flex" justify="space-between">
-          <Col span={24}>
-            <Form.Item>
-              {getFieldDecorator("handle", {
-                rules: [{ required: true, message: "Username is required" }]
-              })(
-                <Input
-                  addonBefore={<Icon type="user" />}
-                  size="large"
-                  placeholder="Set username/handle"
-                />
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row type="flex" justify="space-between">
-          <Col span={11}>
-            <Form.Item>
-              {getFieldDecorator("status", {
-                rules: [{ required: true, message: "Status is required" }]
-              })(
-                <Select size="large" placeholder="Select a status">
-                  <Select.Option key="student" value="student">
-                    Student
-                  </Select.Option>
-                  <Select.Option key="teacher" value="teacher">
-                    Teacher
-                  </Select.Option>
-                </Select>
-              )}
-            </Form.Item>
-          </Col>
-          <Col span={11}>
-            <Form.Item>
-              {getFieldDecorator("githubusername")(
-                <Input
-                  addonBefore={<Icon type="github" />}
-                  size="large"
-                  placeholder="Enter your github username"
-                />
-              )}
-            </Form.Item>
-          </Col>
-          <Col span={24}>
-            <Form.Item>
-              {getFieldDecorator("skills", {
-                rules: [
-                  { required: true, message: "Atleast one skill is required" }
-                ]
-              })(
-                <Input
-                  size="large"
-                  placeholder="Add skills (separated by commas) "
-                />
-              )}
-            </Form.Item>
-          </Col>
-          <Col span={24}>
-            <Form.Item>
-              {getFieldDecorator("location")(
-                <Select size="large" placeholder="Select your location">
-                  {LOCATIONS.map(location => (
-                    <Select.Option key={location} value={location}>
-                      {location}
-                    </Select.Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-          </Col>
-          <Col span={24}>
-            <Card
-              title={
-                <div style={{ fontSize: 20 }}>
-                  <Icon type="book" style={{ marginRight: 15 }} />
-                  <Typography.Text>Academic Details</Typography.Text>
-                </div>
-              }
-            >
-              <Form.Item>
-                {getFieldDecorator("collegeID")(
-                  <Input size="large" placeholder="Enter your College ID" />
-                )}
-              </Form.Item>
-              <Form.Item>
-                {getFieldDecorator("branch")(
-                  <Select size="large" placeholder="Select your Branch">
-                    {BRANCHES.map(branch => (
-                      <Select.Option key={branch} value={branch}>
-                        {branch}
-                      </Select.Option>
-                    ))}
-                  </Select>
-                )}
-              </Form.Item>
-              <Form.Item>
-                {getFieldDecorator("yearOfJoin")(
-                  <Input
-                    size="large"
-                    placeholder="Enter your Year of Joining (YYYY)"
-                  />
-                )}
-              </Form.Item>
-            </Card>
-          </Col>
-          <Button
-            htmlType="submit"
-            type="primary"
-            shape="round"
-            size="large"
-            style={{ marginTop: 25 }}
-          >
-            Create Profile
-          </Button>
-        </Row>
-      </Card> */}
+              Create Profile
+            </Button>
+          </Row>
+        </Card>
+      )}
     </Container>
   );
 };
