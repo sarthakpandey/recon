7b618c65e6c7ce63b68896adcc4d25c609b906b8
proforty7 <proforty7@gmail.com>
2019-10-22 01:06:37 +0530

    should fix
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./components/App";
@@ -9,10 +8,7 @@ import reducers from "./reducers";
 import "antd/dist/antd.css";
 import "./index.css";
 
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
