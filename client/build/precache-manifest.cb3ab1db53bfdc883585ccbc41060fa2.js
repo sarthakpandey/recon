f7b73ad91fb507a88ac36d12d546c6046d7f913a
sarthakpandey <pandeysarthak1@gmail.com>
2019-10-21 23:28:28 +0530

    Last minute changes.
    Signed-off-by: sarthakpandey <pandeysarthak1@gmail.com>

---
+++ client/build/precache-manifest.cb3ab1db53bfdc883585ccbc41060fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa16cbfaf692a6bf53f0e7e9251fbb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508c2840bb121705af13",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "f076d741477486903b84",
+    "url": "/static/css/main.07b84df9.chunk.css"
+  },
+  {
+    "revision": "508c2840bb121705af13",
+    "url": "/static/js/2.f42a5fab.chunk.js"
+  },
+  {
+    "revision": "f076d741477486903b84",
+    "url": "/static/js/main.b387f7f4.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
