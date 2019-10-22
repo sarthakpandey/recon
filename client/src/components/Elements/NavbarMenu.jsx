4a615b8c52b335b5678abe6e0a38a180bc6eba92
proforty7 <proforty7@gmail.com>
2019-10-23 02:12:32 +0530

    fix
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- client/src/components/Elements/NavbarMenu.jsx
+++ client/src/components/Elements/NavbarMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Menu } from "antd";
-import "NavbarMenu.css";
+import "./NavbarMenu.css";
 
 const NavbarMenu = ({ history }) => {
   console.log(history.location.pathname);
