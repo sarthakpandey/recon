4446cbf4c6413546b45c51d23783b3bae2547f3f
proforty7 <proforty7@gmail.com>
2019-10-31 14:20:52 +0530

    add python
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- routes/api/sentimentRouter.js
+++ routes/api/sentimentRouter.js
@@ -4,23 +4,18 @@ const { spawn } = require("child_process");
 const router = require("express").Router();
 
 function runScript() {
-  return spawn("python", ["-u", path.join(__dirname, "myscript.py")]);
+  return spawn("python", [path.join(__dirname, "myscript.py")]);
 }
 
 router.get("/", (req, res) => {
   const subprocess = runScript();
   subprocess.stdout.on("data", data => {
-    console.log(data);
-    res.send(data);
+    console.log(data.toString());
   });
-  subprocess.stderr.on("data", data => {
-    console.log(`error:${data}`);
-    res.send(data);
-  });
-  subprocess.stderr.on("close", () => {
-    console.log("Closed");
-    res.send("closed");
-  });
+  // subprocess.stderr.on("close", () => {
+  //   console.log("Closed");
+  //   res.send("closed");
+  // });
 });
 
 module.exports = router;
